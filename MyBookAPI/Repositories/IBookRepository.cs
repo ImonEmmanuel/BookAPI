4a65a1af6b7293046aeb05706e624a62aa676fa5
Emmanuel <58959180+ImonEmmanuel@users.noreply.github.com>
2022-06-22 13:56:07 +0100

    Add files via upload

---
+++ MyBookAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyBookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task Delete(int id);
+
+        Task Update(Book book);
+    }
+}
